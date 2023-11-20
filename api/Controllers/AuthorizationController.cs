4619cc3c5c5fbf4c7770bfddcfa889bfdf9fa736
Kozak <patrycja.kozak14@gmail.com>
2023-11-21 00:22:07 +0100

    basic setup

---
+++ api/Controllers/AuthorizationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using api.Models;
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+
+        public AuthorizationController()
+        {
+            
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public ActionResult<AuthResponse> Register([FromBody] RegisterModel model)
+        {
+
+
+
+        }
+    }
+}
---
+++ api/Interfaces/IAuthorization.cs
@@ -0,0 +1,12 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Interfaces
+{
+    public interface IAuthorization
+    {
+        public RegisterModel Register([FromBody] RegisterModel model);
+        public string Login([FromBody] LoginModel model);
+        public string CreateToken(Account account);
+    }
+}
