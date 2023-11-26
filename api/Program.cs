50bf9008512d5f7bd001e4cafae45f015e682748
Kozak <patrycja.kozak14@gmail.com>
2023-11-26 22:16:31 +0100

    swagger in all envs

--- api/Program.cs
+++ api/Program.cs
@@ -23,11 +23,10 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
