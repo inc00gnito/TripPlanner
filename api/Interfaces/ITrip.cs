482d381f35cd2f29bec79a1070979540b1cd687e
ToNieJa <mateuszrudko@gmail.com>
2024-01-14 17:35:11 +0100

    merge conflict fix

--- api/Interfaces/ITrip.cs
+++ api/Interfaces/ITrip.cs
@@ -8,7 +8,7 @@ namespace api.Interfaces
         TripPlan CreateTripPlan(int accountId, string startDate, string endDate);
         TripPlan GetTripPlan(int tripPlanId);
         Task<List<TripPlan>> GetAllPublicTripPlans();
-        void AddPlaceToTripPlan(int tripPlanId, int accountId, string placeId);
+        void AddPlaceToTripPlan(int tripPlanId, int accountId, Place place, string chosenDay);
         void DeleteTripPlan(int tripPlanId);
         void RemovePlaceFromTripPlan(string tripPlaceId, int tripPlanId);
         Task<TripPlan> ShareOrUnsharePlanAsync(int planId, int accountId, bool isPublic);
